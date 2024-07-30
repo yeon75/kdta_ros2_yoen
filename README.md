bf8d27ec622c3e0a0c7bd8d6c74f3cde9a3fae8c
yeon75 <yeon007500@gmail.com>
2024-07-30 17:15:16 +0900

    0726

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 ROS2 
 
 #0723 수업 
-VMware 설치, UBUNTU 22.04 설치, github 계정 만들기, terminator 설치, git 설치
-VScode 설치, github 연동
+
+VMware 설치, UBUNTU 22.04 설치, github 계정 만들기, 
+terminator 설치, git 설치, VScode 설치, github 연동
 opencv의 이미지 출력, point, size, rect 등 연산자 활용
 
 #0724 수업 
+
 Matrix class 활용: Scalar를 통한 이미지 색상 지정 및 변경, 이미지 복사 
 행렬에 for문, point, iterator를 적용한 변화 확인 
-VideoCapture를 이용한 카메라 동작(실패)-> video 재생 및 frame 조절 line, putText를 활용하여 line, text를 이미지에 포함시키기
+VideoCapture를 이용한 카메라 동작(실패)
+-> video 재생 및 frame 조절 line, putText를 활용하여 line, text를 이미지에 포함시키기
 
 #0725 수업 
-setMouseCallback을 이용한 마우스 이벤트 코드: 마우스 클릭으로 lenna 이미지에 선 그리기 트랙바 사용: 이미지에 직선 긋기, 명암 조절 등 기능을 탑재한 스크롤 추가 
-FileStorage 클래스: 특정 자료를 index로 정리하여 .json 파일 형식으로 저장/저장한 파일에서 특정 정보만 읽어 화면에 출력 Mask 연산: setTo, copyTo, GRAYSCALE리딩으로 이미지 합성하기
+
+setMouseCallback을 이용한 마우스 이벤트 코드: 마우스 클릭으로 lenna 이미지에 선 그리기 
+트랙바 사용: 이미지에 직선 긋기, 명암 조절 등 기능을 탑재한 스크롤 추가 
+FileStorage 클래스: 특정 자료를 index로 정리하여 .json 파일 형식으로 저장
+저장한 파일에서 특정 정보만 읽어 화면에 출력
+Mask 연산: setTo, copyTo, GRAYSCALE리딩으로 이미지 합성하기
 
--- ch04/ch04Test.cpp
+++ ch04/ch04Test.cpp
@@ -7,8 +7,16 @@ using namespace std;
 void onMouse(int event, int x, int y, int flags, void *data);
 struct Mydata
 {
+    Point ptOld;
+    Point ptNew;
     Mat img;
-
+    Mat background;
+    Scalar white = Scalar(255, 255, 255);
+    Scalar yellow = Scalar(0, 255, 255);
+    Scalar blue = Scalar(255, 0, 0);
+    Scalar green = Scalar(0, 255, 0);
+    Scalar red = Scalar(0, 0, 255);
+    vector<Vec3b> mouseBGR;
     bool flag = false;
 };
 
@@ -17,27 +25,46 @@ String folder = "/home/yeon/ros2/kdta_ros2_yoen/data/";
 int main()
 {
     Mydata mydata;
+    FileStorage fs;
     mydata.img = imread(folder + "lenna.bmp");
+    mydata.background = mydata.img.clone();
 
     namedWindow("img");
-    setMouseCallback("img", flwMouse, (void *)&mydata);
+    setMouseCallback("img", onMouse, (void *)&mydata);
 
     imshow("img", mydata.img);
-    waitKey(0);
+    int keycode = waitKey(0);
+
+    if(keycode == waitKey(0));
+        fs.open(folder + "mouseBGRPoint.josn", FileStorage::WRITE);
+        fs << "mouseBGR" << mydata.mouseBGR;
+        fs.release(); 
 
     return 0;
 }
 
-void flwMouse(int event, int x, int y, int flags, void *data)
+void onMouse(int event, int x, int y, int flags, void *data)
 {
     Mydata *ptr = (Mydata *)data;
-
-    Scalar red = Scalar(0, 0, 255);
-    Mat img(400, 600, CV_8UC3, Scalar(255, 255, 255));
-
     switch (event)
     {
+    case EVENT_LBUTTONDOWN:
+        cout << "mouse left button down" << x << y << endl;
+        ptr->ptOld = Point(x, y);
+        ptr->mouseBGR.push_back(ptr->background.at<Vec3b>(y, x));
+        ptr->flag = true;
+        break;
+    case EVENT_LBUTTONUP:
+        cout << "mouse left button up" << x << y << endl;
+        ptr->flag = false;
+        break;
     case EVENT_MOUSEMOVE:
-        rectangle(img, Rect(50, 50, 100, 70), red, 2);
+        if (ptr->flag & EVENT_FLAG_LBUTTON)
+        {
+            ptr->background.copyTo(ptr->img);
+            rectangle(ptr->img, Rect(Point(x - 25, y - 25), Point(x + 25, y + 25)), ptr->red, 5);
+            imshow("img", ptr->img);
+            ptr->ptOld = Point(x, y);
+        }
     }
 }
